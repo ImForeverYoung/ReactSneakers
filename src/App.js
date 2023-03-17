4af0b8df34ad506d9997694223f56535660d7f33
djuma <adzhumadiev@mail.ru>
2023-03-18 02:49:13 +0800

    init

--- src/App.js
+++ src/App.js
@@ -3,21 +3,87 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className='d-flex justify-between align-center'>
+        <div className="d-flex align-center">
+          <img width={40} height={40} src='logo.png'/>
+          <div className=''>
+            <h3 className='text-uppercase mb-0'>React Sneakers</h3>
+            <p className='opacity-5'>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className='d-flex'>
+          <li className='mr-30'>
+            <img width={18} height={18} src="/images/cart.svg" />
+            <span>1205 KZT</span>
+            
+          </li>
+          <li>
+          <img width={18} height={18} src="/images/like.svg" />
+          </li>
+          <li>
+          <img width={18} height={18} src="/images/user.png" />
+          </li>
+        </ul>
       </header>
+      <div className='content p-40'>
+        <h1 className='mb-40'>Все кроссовки</h1> 
+        <div className='sneakers d-flex '>
+          <div className='card'>
+            <img width={133} height={112} src="/images/image1.jpg" alt=""/>
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Price:</span>
+                <b>22 990 KZT</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src="/images/plus.svg" />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+          <img width={133} height={112} src="/images/image2.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className='d-flex justify-between align-center'>
+            <div className='d-flex flex-column'>
+              <span>Price:</span>
+              <b>22 990 KZT</b>
+            </div>
+            <button className='button'>
+              <img width={11} height={11} src="/images/plus.svg" />
+            </button>
+          </div>
+          </div>
+          <div className='card'>
+          <img width={133} height={112} src="/images/image3.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className='d-flex justify-between align-center'>
+            <div className='d-flex flex-column'>
+              <span>Price:</span>
+              <b>22 990 KZT</b>
+            </div>
+            <button className='button'>
+              <img width={11} height={11} src="/images/plus.svg" />
+            </button>
+          </div>
+          </div>
+          <div className='card'>
+          <img width={133} height={112} src="/images/image4.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className='d-flex justify-between align-center'>
+            <div className='d-flex flex-column'>
+              <span>Price:</span>
+              <b>22 990 KZT</b>
+            </div>
+            <button className='button'>
+              <img width={11} height={11} src="/images/plus.svg" />
+            </button>
+          </div>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
+
+import 'macro-css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
